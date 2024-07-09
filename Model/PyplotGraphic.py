2d18398e4e0c6f81d1b433d2f729c2ecbc1ee92f
zazemlil <tiknik19@mail.ru>
2024-07-09 15:25:18 +0300

    simplification of the function

--- Model/PyplotGraphic.py
+++ Model/PyplotGraphic.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.pylab as plab 
 from numpy import *
+from sympy import symbols, Eq, solve
 
 from Model.Graphic import Graphic
 
@@ -18,30 +19,40 @@ class PyplotGraphic(Graphic):
             
             # adding graphs
             for item in self._funcs:
-                y, x = self._exec_func(item)
-                if item[0] == 'y':
-                    print(1)
+                # simplification of the function
+                solved_item = self._solve_func(item)
+
+                # execution of the function
+                y, x = self._exec_func(solved_item)
+                if solved_item[0] == 'y':
                     if isinstance(y, int) or isinstance(y, float):
-                        print(2)
                         y = [y for i in range(len(self._x))]
                     axes.plot(self._x, y)
-                elif item[0] == 'x':
-                    print(3)
+                elif solved_item[0] == 'x':
                     if isinstance(x, int) or isinstance(x, float):
-                        print(4)
                         x = [x for i in range(len(self._x))]
                     axes.plot(x, self._x)
 
-            # configure graphic
+            # configure and show graphic 
             self._configure_graphic(axes)
-            # showing graphic
             plt.show()
         except Exception as error:
-            #print(f"{error=}, {type(error)=}")
             self.notify({"error": str(error), "func": str(item), "index": self._funcs.index(item)})
                 
-    def _exec_func(self, item) -> list[dict, None] | list[None, dict]:
+    def _solve_func(self, item) -> str:
+        x, y = symbols('x y')
+        item_half_1, item_half_2 = item[ : item.find("=")], item[item.find("=") + 1 : ]
+        equation = Eq(eval(item_half_1), eval(item_half_2))
+        if item.count('y') > 0:
+            solution = solve(equation, y)
+            solution = str(solution[0]).replace(" ", "")
+            return "y="+solution
+        elif item.count('x') > 0:
+            solution = solve(equation, x)
+            solution = str(solution[0]).replace(" ", "")
+            return "x="+solution
 
+    def _exec_func(self, item) -> list[dict, None] | list[None, dict]:
         if item[0] == 'y':
             x = self._x
             ldic = locals()
@@ -54,15 +65,6 @@ class PyplotGraphic(Graphic):
             with errstate(divide='ignore', invalid='ignore'):
                 exec(item, globals(), ldic)
                 return None, ldic['x']
-            
-        # x = self._x
-        # ldic = locals()
-        # with errstate(divide='ignore', invalid='ignore'):
-        #     exec(item, globals(), ldic)
-        #     if "y" in ldic:
-        #         return ldic['y'], None
-        #     else:
-        #         return None, ldic['x']
 
     def _configure_graphic(self, axes) -> None:
         fig = plab.gcf()
